cd30e69022f6e810a3f5fb30b521b1b275358627
EsmeraldVL <86280015+EsmeraldVL@users.noreply.github.com>
2021-10-08 17:22:18 -0600

    Script para base de datos

--- MOVI_DB.sql
+++ MOVI_DB.sql
@@ -32,6 +32,7 @@ CREATE TABLE `CARRITO` (
   `fecha` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+
 -- --------------------------------------------------------
 
 --
