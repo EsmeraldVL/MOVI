e5c94ab25be6086744326b6bc83a961219a9fde1
Astrid Martinez <47261408+AstridGMC@users.noreply.github.com>
2021-10-27 00:46:32 -0600

    parte2

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Lo sentimos, Las credenciales no coinciden con nuestros registros.',
+    'password' => 'El password es incorrecto.',
+    'throttle' => 'Demasiados intentos de inicio de sesion. Por favor intente en  :seconds segundos.',
 
 ];
